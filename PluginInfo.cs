a3c8057be281191df805e599189960bf53e23a15
Pascal PEREZ <42294009+talesofpixelia@users.noreply.github.com>
2021-06-25 15:55:54 +0200

    Initial commit

---
+++ PluginInfo.cs
@@ -0,0 +1,26 @@
+﻿using SRTPluginBase;
+using System;
+
+namespace SRTPluginProviderDR1
+{
+    internal class PluginInfo : IPluginInfo
+    {
+        public string Name => "Game Memory Provider (Dead Rising (2016))";
+
+        public string Description => "A game memory provider plugin for Dead Rising (2016).";
+
+        public string Author => "Squirrelies & VideoGameRoulette";
+
+        public Uri MoreInfoURL => new Uri("https://github.com/talesofpixelia/SRTPluginProviderDR1");
+
+        public int VersionMajor => assemblyVersion.Major;
+
+        public int VersionMinor => assemblyVersion.Minor;
+
+        public int VersionBuild => assemblyVersion.Build;
+
+        public int VersionRevision => assemblyVersion.Revision;
+
+        private Version assemblyVersion = System.Reflection.Assembly.GetExecutingAssembly().GetName().Version;
+    }
+}
