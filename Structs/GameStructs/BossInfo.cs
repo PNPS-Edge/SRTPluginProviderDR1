78ec2e4c91d35f13ec165bde91cca05f73b40e6d
Christopher D Couture <videogameroulette@yahoo.ca>
2021-07-29 08:03:59 -0400

    update boss info
    added IsAlive, IsDamaged, and Percentage props

--- Structs/GameStructs/BossInfo.cs
+++ Structs/GameStructs/BossInfo.cs
@@ -12,6 +12,10 @@ namespace SRTPluginProviderDR1.Structs.GameStructs
         [FieldOffset(0x12E8)] public int MaxHealth;
         [FieldOffset(0x12EC)] public int CurrentHealth;
 
+        public bool IsAlive => MaximumHP > 0 && CurrentHP > 0 && CurrentHP <= MaximumHP;
+        public bool IsDamaged => IsAlive && CurrentHP < MaximumHP;
+        public float Percentage => IsAlive ? (float)CurrentHP / (float)MaximumHP : 0f;
+        
         public static BossInfo AsStruct(byte[] data)
         {
             fixed (byte* pb = &data[0])
