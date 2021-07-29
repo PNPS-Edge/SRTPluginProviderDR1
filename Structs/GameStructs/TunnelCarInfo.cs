9309bb32885e5513b32daaba5268f7283411ae1e
Christopher D Couture <videogameroulette@yahoo.ca>
2021-07-29 10:20:39 -0400

    added health state to car info

--- Structs/GameStructs/TunnelCarInfo.cs
+++ Structs/GameStructs/TunnelCarInfo.cs
@@ -10,5 +10,27 @@ namespace SRTPluginProviderDR1.Structs.GameStructs
 
         public int MaxHealth => maxHealth;
         public int CurrentHealth => currentHealth;
+        public bool IsAlive => CurrentHealth != 0 && CurrentHealth > 0 && CurrentHealth <= MaxHealth;
+        public float Percentage => IsAlive ? (float)CurrentHealth / (float)MaxHealth : 0f;
+
+        public enum PlayerStatus
+        {
+            Dead,
+            Fine,
+            FineToo,
+            Caution,
+            Danger,
+        }
+
+        public PlayerStatus HealthState
+        {
+            get =>
+                !IsAlive ? PlayerStatus.Dead :
+                Percentage >= 0.75 ? PlayerStatus.Fine :
+                Percentage >= 0.50 ? PlayerStatus.FineToo :
+                Percentage >= 0.25 ? PlayerStatus.Caution : PlayerStatus.Danger;
+        }
+
+        public string CurrentHealthState => HealthState.ToString();
     }
 }
